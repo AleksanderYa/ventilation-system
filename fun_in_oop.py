fa1649187707b3f8962614dc587a1b849ecd971e
AleksanderYa <250000ye@gmail.com>
2021-12-24 17:05:38 +0200

    update class Fun()

--- fun_in_oop.py
+++ fun_in_oop.py
@@ -9,15 +9,35 @@ import time
 
 
 class Fun:
-    def __init__(self, pin):
-        pin_ = Pin(pin, Pin.OUT)
-        pin_control = Signal(pin_, invert=True)
-    
-    
+
+
+    def __init__(self, pin: int):
+        _pin_ = Pin(pin, Pin.OUT)
+        # to normally open output -> invert=True
+        self._pin_control = Signal(_pin_, invert=True)
+
+    def on_fun(self):
+        self._pin_control.on()
+        return f"Fun {1} on"
+
+    def off_fun(self):
+        self._pin_control.off()
+        return f"Fun {1} off"
+
+
+class Fun_toilet(Fun):
+    pass
+
+class Fun_bathroom(Fun):
+    pass
+
+class Fun_out(Fun):
+    pass
+
 class DHT:
-"""
-    DHT (Digital Humidity & Temperature)
-"""
+    """
+        DHT (Digital Humidity & Temperature)
+    """
     def __init__(self, pin):
         pass
     
